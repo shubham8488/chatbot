79b9f073b2d9a01ed12d82664cc55d988e5dad74
Shubham Kumar <shubham.8488@gmail.com>
2019-01-31 21:58:33 +0530

    abc

--- chat_app/alpha/urls.py
+++ chat_app/alpha/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Home, name='home'),
-    url(r'^post/$', views.Post, name='post'),
+    path('', views.Home, name='home'),
+    path('post/', views.Post, name='post'),
 ]
